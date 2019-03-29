bdf2763f43fb16fff6dda45073dcc221e8766dda
Christian <christian.sihotang23@gmail.com>
2019-03-29 10:29:00 +0700

    update

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -9,6 +9,12 @@ set -x
 npm run build
 set +x
 
+echo 'Maven Build Package'
+echo 'the file ".pidfile".'
+set -x
+mvn package
+set +x
+
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
 echo 'The "npm start" command has a trailing ampersand so that the command runs'
